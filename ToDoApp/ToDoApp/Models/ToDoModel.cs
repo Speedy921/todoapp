f6e92809cb64737fce8609cfc575de5219be25f4
Artem Viatkin <newyorkbox47@gmail.com>
2022-04-07 18:16:08 +0300

    first commit

---
+++ ToDoApp/ToDoApp/Models/ToDoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    class ToDoModel
+    {
+        private bool _IsDone;
+        private string _Text;
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+               
+        public bool IsDone
+        {
+            get { return _IsDone; }
+            set { _IsDone = value; }
+        }     
+
+        public string Text
+        {
+            get { return _Text; }
+            set { _Text = value; }
+        }
+
+
+    }
+}
